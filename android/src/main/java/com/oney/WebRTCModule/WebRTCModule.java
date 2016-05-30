9005c003033d0b5c565a98b12136cbfa84b5d92e
paweldomas <pawel.domas@jitsi.org>
2016-05-30 14:04:55 +0200

    Log camera error on Android

--- android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
+++ android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
@@ -592,7 +592,7 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
         return VideoCapturerAndroid.create(name, new VideoCapturerAndroid.CameraErrorHandler() {
             @Override
             public void onCameraError(String s) {
-
+                Log.e(TAG, "onCameraError: " + s);
             }
         });
     }
